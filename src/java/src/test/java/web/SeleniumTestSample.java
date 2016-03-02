90595f9dd6cccb38000433bf3fe07c445d0787fd
pollseed <cb.so.devope+0001@gmail.com>
2016-03-02 16:24:39 +0900

    Add Firefox

--- src/java/src/test/java/web/SeleniumTestSample.java
+++ src/java/src/test/java/web/SeleniumTestSample.java
@@ -1,5 +1,7 @@
 package web;
 
+import java.util.Arrays;
+
 import junit.framework.TestCase;
 
 import org.junit.Test;
@@ -7,25 +9,31 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SeleniumTestSample extends TestCase {
     @Test
     public void test_request() {
-        WebDriver driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver.exe");
+        final WebDriver[] drivers = new WebDriver[] { new ChromeDriver(), new FirefoxDriver() };
+        Arrays.stream(drivers).forEach(driver -> testBrowser(driver).quit());
+    }
+
+    private static final WebDriver testBrowser(final WebDriver driver) {
         driver.get("http://www.google.com");
-        WebElement element = driver.findElement(By.name("q"));
+        final WebElement element = driver.findElement(By.name("q"));
         element.sendKeys("Cheese!");
         element.submit();
         System.out.println(driver.getTitle());
         new WebDriverWait(driver, 10).until(new ExpectedCondition<Boolean>() {
             @Override
-            public Boolean apply(WebDriver arg0) {
+            public Boolean apply(final WebDriver arg0) {
                 return arg0.getTitle().toLowerCase().startsWith("cheese!");
             }
         });
         System.out.println(driver.getTitle());
-        driver.quit();
+        return driver;
     }
 }
